af7e1b390dfea34c8344be830dc17b6c67da9ecf
Chia Wei Hsu <68754706+chiaweihsu@users.noreply.github.com>
2020-07-28 20:11:15 +0800

    Update example script

--- example_script.m
+++ example_script.m
@@ -1,12 +1,12 @@
 % A script giving examples of using the cal_smatrix_RGF() function.
 
 %% system parameters of this example
-rng default % for reproducibility of random numbers
-ny = 100; nx = 100;  % system size
-syst.kdx = 2*pi/10;  % normalized frequency; 10 grid points per vacuum wavelength
-syst.epsilon_L = 1.0;  % relative permittivity on the left
-syst.epsilon_R = 1.0;  % relative permittivity on the right
-syst.epsilon = 1.0 + 2.0*rand(ny, nx);  % relative permittivity of the scattering region; can be complex-valued
+rng default % for reproducibility of random numbers below
+ny = 100; nx = 100;  % number of grid points in x and y
+syst.kdx = 2*pi/10;  % kdx = omega/c*dx where dx is grid size; use 10 grid points per vacuum wavelength here
+syst.epsilon_L = 1.0;  % relative permittivity for the homogeneouse space on the left
+syst.epsilon_R = 1.0;  % relative permittivity for the homogeneouse space on the right
+syst.epsilon = 1.0 + 2.0*rand(ny, nx);  % relative permittivity of the scattering region; can be anything and can be complex-valued; use random numbers as an example
 syst.BC = 'periodic';  % boundary condition in y
 
 %% calculate the transmission matrix from left to right
@@ -40,13 +40,13 @@ t = S(N_prop_L + (1:N_prop_R), 1:N_prop_L); % L->R
 tp = S(1:N_prop_L, N_prop_L + (1:N_prop_R)); % R->L
 rp = S(N_prop_L + (1:N_prop_R), N_prop_L + (1:N_prop_R)); % R->R
 
-%% calculate selected columns (incident angles) of the transmission and the reflection matrices
-% find the channel indices for the desired incident angles
+%% calculate selected columns (incident angles) of the transmission and reflection matrices
+% find the channel indices for a few incident angles of interest
 angles_in_deg = [-45, 0, 45];
 kxdx = channels.L.kxdx(channels.L.ind_prop);
 kydx = channels.kydx(channels.L.ind_prop);
 ind = round(interp1(atan2(kydx, kxdx), 1:N_prop_L, angles_in_deg/180*pi));
-% specify those as incident channels from the left
+% specify those as the input channels from the left
 in = []; in.ind_in_L = ind;
 out = {'left', 'right'};
 fprintf('computing selected columns of the scattering matrix...\n');
